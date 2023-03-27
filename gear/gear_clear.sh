850cad2fb16c3454e8eed33dd27484f4bc79a26a
wladie <wladie.miller.dodo@gmail.com>
2023-03-27 03:27:29 +0300

    upd

--- gear/gear_clear.sh
+++ gear/gear_clear.sh
@@ -4,13 +4,13 @@ echo "--------------------------------------------------------------------------
 curl -s https://raw.githubusercontent.com/fromIziDAO/things/main/logo.sh | bash
 echo "-----------------------------------------------------------------------------"
 if [ -z $NODENAME_GEAR ]; then
-        read -p "Введите ваше имя ноды (только буквы): " NODENAME_GEAR
+        read -p "Введите ваше имя ноды (без спецсимволов - только буквы и цифры): " NODENAME_GEAR
         echo 'export NODENAME='$NODENAME_GEAR >> $HOME/.profile
 fi
 echo 'Ваше имя ноды: ' $NODENAME_GEAR
 sleep 1
 echo "-----------------------------------------------------------------------------"
-echo "Starting soft"
+echo "Устанавливаем софт"
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/fromIziDAO/things/main/ufw.sh | bash &>/dev/null
 curl -s https://raw.githubusercontent.com/fromIziDAO/things/main/rust.sh | bash &>/dev/null
@@ -20,7 +20,7 @@ source $HOME/.profile &>/dev/null
 source $HOME/.bashrc &>/dev/null
 source $HOME/.cargo/env &>/dev/null
 sleep 1
-echo "Soft done"
+echo "софт установлен"
 echo "-----------------------------------------------------------------------------"
 
 
@@ -28,7 +28,7 @@ wget https://get.gear.rs/gear-nightly-linux-x86_64.tar.xz &>/dev/null
 tar xvf gear-nightly-linux-x86_64.tar.xz &>/dev/null
 rm gear-nightly-linux-x86_64.tar.xz &>/dev/null
 chmod +x $HOME/gear &>/dev/null
-echo "Build done"
+echo "Билд завершен успешно"
 echo "-----------------------------------------------------------------------------"
 
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
@@ -49,8 +49,7 @@ ExecStart=$HOME/gear \
         --name $NODENAME_GEAR \
         --execution wasm \
 	--port 31333 \
-        --telemetry-url 'ws://telemetry-backend-shard.gear-tech.io:32001/submit 0' \
-	--telemetry-url 'wss://telemetry.postcapitalist.io/submit 0'
+        --telemetry-url 'ws://telemetry-backend-shard.gear-tech.io:32001/submit 0'	
 Restart=always
 RestartSec=10
 LimitNOFILE=10000
@@ -60,12 +59,12 @@ WantedBy=multi-user.target
 EOF
 
 
-echo "Service files created"
+echo "Сервисные файлы созданы"
 echo "-----------------------------------------------------------------------------"
 sudo systemctl restart systemd-journald &>/dev/null
 sudo systemctl daemon-reload &>/dev/null
 sudo systemctl enable gear &>/dev/null
 sudo systemctl restart gear &>/dev/null
 
-echo "Node started"
+echo "Нода запущена"
 echo "-----------------------------------------------------------------------------"
